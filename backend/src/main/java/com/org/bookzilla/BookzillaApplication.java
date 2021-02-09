b5aab1aa808fdf59af77637e9c15b8eda11a5272
Pooja Khanal <khanalpooja425@gmail.com>
2021-02-08 23:07:03 -0500

    Initial commit

---
+++ backend/src/main/java/com/org/bookzilla/BookzillaApplication.java
@@ -0,0 +1,13 @@
+package com.org.bookzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzillaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/org/bookzilla/BookzillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.bookzilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
